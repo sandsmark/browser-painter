9610c7dca00f299d7b80dd20caf1a37fda79f83d
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2021-04-30 00:37:19 +0200

    blah

--- sketch.js
+++ sketch.js
@@ -4,6 +4,40 @@
     var canvas;
     var ctx;
 
+    let isDrawing = false;
+    let lastX = 0;
+    let lastY = 0;
+
+    function draw(e) {
+        if (!isDrawing){
+            [lastX, lastY] = [e.offsetX, e.offsetY];
+            return;
+        }
+
+        ctx.quadraticCurveTo(lastX, lastY, (lastX + e.offsetX) / 2, (lastY + e.offsetY) / 2);
+        ctx.stroke();
+
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+    }
+    function onMouseUp(e) {
+        isDrawing = false;
+
+        // In case we just got a single point
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+    }
+    function onMouseDown(e) {
+        isDrawing = true;
+        ctx.strokeStyle = drawConfig.color;
+        ctx.lineWidth = drawConfig.weight;
+        ctx.beginPath();
+        ctx.moveTo(e.offsetX, e.offsetY);
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+    }
+    function onMouseOut(e) {
+        isDrawing = false;
+    }
+
     function startDraw() {
         canvas = document.createElement('canvas');
         const body = document.body;
@@ -13,13 +47,12 @@
         let windowHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
 
 
-        console.log(windowWidth)
         canvas.width = windowWidth * scale;
         canvas.height = windowHeight * scale;
 
         Object.assign(canvas.style, {
             position:"absolute", 
-            zIndex: 1000, 
+            zIndex: 1000000000, 
             // pointerEvents: 'none',
             // height: windowHeight + '!important',
             width: windowWidth + 'px',
@@ -34,51 +67,32 @@
         ctx.scale(scale, scale)
         ctx.translate(0.5, 0.5);
 
-        let isDrawing = false;
-        let lastX = 0;
-        let lastY = 0;
-
-        function draw(e) {
-            if (!isDrawing){
-                [lastX, lastY] = [e.offsetX, e.offsetY];
-                return;
-            }
-
-            ctx.quadraticCurveTo(lastX, lastY, (lastX + e.offsetX) / 2, (lastY + e.offsetY) / 2);
-            ctx.stroke();
-
-            [lastX, lastY] = [e.offsetX, e.offsetY];
-        }
-
-        canvas.addEventListener('mousedown', (e) => {
-            isDrawing = true;
-            ctx.strokeStyle = drawConfig.color;
-            ctx.lineWidth = drawConfig.weight;
-            ctx.beginPath();
-            ctx.moveTo(e.offsetX, e.offsetY);
-            [lastX, lastY] = [e.offsetX, e.offsetY];
-        });
-
+        canvas.addEventListener('mousedown', onMouseDown);
         canvas.addEventListener('mousemove', draw);
-        canvas.addEventListener('mouseup', function(e) {
-            ctx.lineTo(e.offsetX, e.offsetY);
-            ctx.stroke();
-            isDrawing = false;
-        });
-        canvas.addEventListener('mouseout', () => isDrawing = false);
+        canvas.addEventListener('mouseup', onMouseUp);
+        canvas.addEventListener('mouseout', onMouseOut);
         initialized = true;
     }
 
     chrome.runtime.onMessage.addListener(function(msg) {
-        console.log(msg);
         if (msg.command === 'stop') {
-            canvas.remove();
-            initialized = false;
+            if (!initialized) {
+                return;
+            }
+            //canvas.removeEventListener('mousedown', onMouseDown);
+            //canvas.removeEventListener('mousemove', draw);
+            //canvas.removeEventListener('mouseup', onMouseUp);
+            //canvas.removeEventListener('mouseout', onMouseOut);
+            canvas.style.pointerEvents = 'none';
+            //canvas.remove();
+            //initialized = false;
             return;
         }
 
         if (!initialized) {
             startDraw();
+        } else {
+            canvas.style.pointerEvents = 'auto';
         }
         drawConfig = msg.drawConfig;
         if (msg.command === 'clear') {
